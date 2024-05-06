c58b02ce9abf88a4c939311ed8fccc7e20178de3
Gabriel Magalhães <magluz@live.com>
2024-05-05 21:24:15 -0300

    ajustes

---
+++ src/components/AppMenubox.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="d-flex align-start flex-row overflow-x-auto">
+    <v-card width="135" class="ma-2 pa-2 text-center" v-for="(tile, i) in tiles" :key="i" :value="tile">
+      <v-img :src="tile.img" width="40" class="px-2 m-2"></v-img>
+      <v-card-text>
+        {{ tile.title }}
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    sheet: false,
+    selected: '',
+    tiles: [
+      { img: new URL("@/assets/icons/icon_mn_participante.svg", import.meta.url).href, title: 'Participante' },
+      { img: new URL("@/assets/icons/icon_mn_programacao.svg", import.meta.url).href, title: 'Programação' },
+      { img: new URL("@/assets/icons/icon_mn_mapa.svg", import.meta.url).href, title: 'Mapa' },
+      { img: new URL("@/assets/icons/icon_mn_patrocinio.svg", import.meta.url).href, title: 'Patrocínio' }
+    ],
+  }),
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-   
-    
-  <HelloWorld />
+  <v-container>
+    <v-responsive class="align-centerfill-height mx-auto">
+      <v-card class="mx-auto" variant="outlined" border="none">
+        <v-img class="align-end text-white" rounded="xl" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+         </v-img>
+        <v-card-subtitle class="pt-4">
+          XVII  Seminário Técnico de Proteção e Controle - STPC
+        </v-card-subtitle>
+        <v-card-text>
+          De 14 a 17 de outubro de 2024, em São Paulo
+        </v-card-text>
+      </v-card>
+      <AppMenubox />
+    </v-responsive>
+  </v-container>
+  <!-- <HelloWorld /> -->
 </template>
 
 <script setup>
-  //
+//
 </script>
